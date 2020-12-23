2ae1c0eaa5e1cfa8b7c71794071baf21294a1777
J.P. Hutchins <jphutchins@gmail.com>
2020-12-22 19:52:10 -0800

    Remove unneeded Raises, add test

--- pyavreceiver/command.py
+++ pyavreceiver/command.py
@@ -8,39 +8,32 @@ class TelnetCommand(ABC):
     @abstractmethod
     def set_val(self, val) -> str:
         """Format the command with argument and return."""
-        raise NotImplementedError
 
     @abstractmethod
     def set_query(self) -> str:
         """Format the command with query and return."""
-        raise NotImplementedError
 
     @property
     @abstractmethod
     def command(self) -> str:
         """The command portion of the message."""
-        raise NotImplementedError
 
     @property
     @abstractmethod
     def message(self) -> str:
         """The complete message; command + argument."""
-        raise NotImplementedError
 
     @property
     @abstractmethod
     def name(self) -> str:
         """The name of the command."""
-        raise NotImplementedError
 
     @property
     @abstractmethod
     def val(self) -> str:
         """The argument of the command."""
-        raise NotImplementedError
 
     @property
     @abstractmethod
     def values(self) -> list:
         """Return the valid argument values."""
-        raise NotImplementedError
---
+++ tests/test_command.py
@@ -0,0 +1,10 @@
+"""Tests for command base classes."""
+import pytest
+
+from pyavreceiver.command import TelnetCommand
+
+def test_telnet_command():
+    """Test telnet command abstract base class."""
+    with pytest.raises(TypeError):
+        # pylint: disable=abstract-class-instantiated
+        TelnetCommand()
