23a6377d8b920f38d895589cce37c8b4aa2c1c86
J.P. Hutchins <jphutchins@gmail.com>
2020-12-22 20:00:49 -0800

    black

--- tests/test_command.py
+++ tests/test_command.py
@@ -3,6 +3,7 @@ import pytest
 
 from pyavreceiver.command import TelnetCommand
 
+
 def test_telnet_command():
     """Test telnet command abstract base class."""
     with pytest.raises(TypeError):
