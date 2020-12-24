b978c2879eaf197ab101d31e166bc0cef9469991
J.P. Hutchins <jphutchins@gmail.com>
2020-12-23 18:57:03 -0800

    Sort attr and setters alphabetically

--- pyavreceiver/zone.py
+++ pyavreceiver/zone.py
@@ -41,127 +41,140 @@ class Zone:
         """The dictionary of available commands."""
         return self._commands
 
+    # Universal Audio Video Receiver attributes -------------------------------
+    # in alphabetical order by property
+    # setters prefixed with set_ and grouped below their property
+
     @property
     def bass(self) -> int:
         """The state of bass."""
         return self.get(const.ATTR_BASS)
 
-    @property
-    def power(self) -> str:
-        """The state of power."""
-        return self.get(const.ATTR_POWER)
-
-    @property
-    def volume(self) -> str:
-        """The state of volume."""
-        return self.get(const.ATTR_VOLUME)
-
-    @property
-    def lfe_level(self) -> str:
-        """The state of LFE level."""
-        return self.get(const.ATTR_LFE_LEVEL)
-
-    @property
-    def meta_dynamic_range(self) -> str:
-        """The state of metadata dynamic range control."""
-        return self.get(const.ATTR_META_DRC)
-
-    @property
-    def mute(self) -> str:
-        """The state of mute."""
-        return self.get(const.ATTR_MUTE)
-
-    @property
-    def source(self) -> str:
-        """The state of source."""
-        return self.get(const.ATTR_SOURCE)
-
-    @property
-    def tone_control(self) -> str:
-        """The state of tone control."""
-        return self.get(const.ATTR_TONE_CONTROL)
-
-    @property
-    def treble(self) -> int:
-        """The state of treble."""
-        return self.get(const.ATTR_TREBLE)
-
-    @property
-    def subwoofer_one(self) -> bool:
-        """The state of subwoofer one."""
-        return self.get(const.ATTR_SUBWOOFER_ONE)
+    def set_bass(self, val: float) -> bool:
+        """Request the receiver set the bass to val."""
+        return self.set(const.ATTR_BASS, val)
 
     @property
     def dialog_level(self) -> str:
         """The state of dialog level."""
         return self.get(const.ATTR_DIALOG_LEVEL)
 
+    def set_dialog_level(self, val) -> bool:
+        """Request the receiver set dialog level to val."""
+        return self.set(const.ATTR_DIALOG_LEVEL, val)
+
     @property
     def dsp_dynamic_range(self) -> str:
         """The state of DSP dynamic range."""
         return self.get(const.ATTR_DSP_DRC)
+
+    def set_dsp_dynamic_range(self, val) -> bool:
+        """Request the receiver set the DRC to val."""
+        return self.set(const.ATTR_DSP_DRC, val)
 
     @property
     def dsp_mode(self) -> str:
         """The state of DSP mode."""
         return self.get(const.ATTR_DSP_MODE)
 
-    def volume_down(self) -> bool:
-        """Request the receiver turn the volume down."""
-        return self.set(const.ATTR_VOLUME_DOWN)
-
-    def volume_up(self) -> bool:
-        """Request the receiver turn the volume up."""
-        return self.set(const.ATTR_VOLUME_UP)
-
-    def set_dialog_level(self, val) -> bool:
-        """Request the receiver set dialog level to val."""
-        return self.set(const.ATTR_DIALOG_LEVEL, val)
-
-    def set_dsp_dynamic_range(self, val) -> bool:
-        """Request the receiver set the DRC to val."""
-        return self.set(const.ATTR_DSP_DRC, val)
-
     def set_dsp_mode(self, val: str) -> bool:
         """Request the receiver set DSP mode to val."""
         return self.set(const.ATTR_DSP_MODE, val)
 
-    def set_power(self, val: bool) -> bool:
-        """Request the receiver set power to val."""
-        return self.set(const.ATTR_POWER, val)
-
-    def set_volume(self, val: float) -> bool:
-        """Request the receiver set volume to val."""
-        return self.set(const.ATTR_VOLUME, val)
-
-    def set_meta_dynamic_range(self, val: str) -> bool:
-        """Request the receiver set meta DRC to val."""
-        return self.set(const.ATTR_META_DRC, val)
+    @property
+    def lfe_level(self) -> str:
+        """The state of LFE level."""
+        return self.get(const.ATTR_LFE_LEVEL)
 
     def set_lfe_level(self, val: float) -> bool:
         """Request the receiver set LFE level to val."""
         return self.set(const.ATTR_LFE_LEVEL, val)
 
+    @property
+    def meta_dynamic_range(self) -> str:
+        """The state of metadata dynamic range control."""
+        return self.get(const.ATTR_META_DRC)
+
+    def set_meta_dynamic_range(self, val: str) -> bool:
+        """Request the receiver set meta DRC to val."""
+        return self.set(const.ATTR_META_DRC, val)
+
+    @property
+    def mute(self) -> str:
+        """The state of mute."""
+        return self.get(const.ATTR_MUTE)
+
     def set_mute(self, val: bool) -> bool:
         """Request the receiver set mute to val."""
         return self.set(const.ATTR_MUTE, val)
 
-    def set_source(self, val: str) -> bool:
-        """Request the receiver set the source to val."""
-        return self.set(const.ATTR_SOURCE, val)
+    @property
+    def power(self) -> str:
+        """The state of power."""
+        return self.get(const.ATTR_POWER)
 
-    def set_subwoofer_one(self, val: bool) -> bool:
-        """Request the receiver set subwoofer one to val."""
-        return self.set(const.ATTR_SUBWOOFER_ONE, val)
+    def set_power(self, val: bool) -> bool:
+        """Request the receiver set power to val."""
+        return self.set(const.ATTR_POWER, val)
+
+    @property
+    def soundmode(self) -> str:
+        """The state of soundmode."""
+        return self.get(const.ATTR_SOUND_MODE)
 
     def set_soundmode(self, val: str) -> bool:
         """Request the receiver set the sound mode to val."""
         return self.set(const.ATTR_SOUND_MODE, val)
 
+    @property
+    def source(self) -> str:
+        """The state of source."""
+        return self.get(const.ATTR_SOURCE)
+
+    def set_source(self, val: str) -> bool:
+        """Request the receiver set the source to val."""
+        return self.set(const.ATTR_SOURCE, val)
+
+    @property
+    def subwoofer_one(self) -> bool:
+        """The state of subwoofer one."""
+        return self.get(const.ATTR_SUBWOOFER_ONE)
+
+    def set_subwoofer_one(self, val: bool) -> bool:
+        """Request the receiver set subwoofer one to val."""
+        return self.set(const.ATTR_SUBWOOFER_ONE, val)
+
+    @property
+    def tone_control(self) -> str:
+        """The state of tone control."""
+        return self.get(const.ATTR_TONE_CONTROL)
+
+    def set_tone_control(self, val: bool) -> bool:
+        """Request the receiver set the treble to val."""
+        return self.set(const.ATTR_TONE_CONTROL, val)
+
+    @property
+    def treble(self) -> int:
+        """The state of treble."""
+        return self.get(const.ATTR_TREBLE)
+
     def set_treble(self, val: float) -> bool:
         """Request the receiver set the treble to val."""
         return self.set(const.ATTR_TREBLE, val)
 
-    def set_bass(self, val: float) -> bool:
-        """Request the receiver set the bass to val."""
-        return self.set(const.ATTR_BASS, val)
+    @property
+    def volume(self) -> str:
+        """The state of volume."""
+        return self.get(const.ATTR_VOLUME)
+
+    def set_volume(self, val: float) -> bool:
+        """Request the receiver set volume to val."""
+        return self.set(const.ATTR_VOLUME, val)
+
+    def set_volume_down(self) -> bool:
+        """Request the receiver turn the volume down."""
+        return self.set(const.ATTR_VOLUME_DOWN)
+
+    def set_volume_up(self) -> bool:
+        """Request the receiver turn the volume up."""
+        return self.set(const.ATTR_VOLUME_UP)
