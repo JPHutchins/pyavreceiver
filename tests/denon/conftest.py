0290a709436402d4ef3ebc087c78aa758eb740b4
J.P. Hutchins <jphutchins@gmail.com>
2020-12-24 17:18:20 -0800

    Remove tests/commands.yaml and add fake entries in fixture

--- tests/denon/conftest.py
+++ tests/denon/conftest.py
@@ -8,14 +8,30 @@ from pyavreceiver.denon.response import DenonMessage
 @pytest.fixture(name="message_none")
 def denon_message_none():
     """Create an empty DenonMessage."""
-    with open("tests/denon/commands.yaml") as file:
+    with open("pyavreceiver/denon/commands.yaml") as file:
         command_dict = yaml.safe_load(file.read())
+        command_dict.update(fake_entries)
     return DenonMessage(None, command_dict)
 
 
 @pytest.fixture(name="command_dict")
 def denon_command_dict():
     """Create the command dict."""
-    with open("tests/denon/commands.yaml") as file:
+    with open("pyavreceiver/denon/commands.yaml") as file:
         command_dict = yaml.safe_load(file.read())
+        command_dict.update(fake_entries)
     return command_dict
+
+fake_entries = {
+    "FAKEFORTEST": {},
+    "FAKE": {
+        "FOR TESTS": {}
+    },
+    "FAKEFO": {
+        "R T E STS": {}
+    },
+    "FAKEN": {
+        "^params": True,
+        "OR": "TEST"
+    }
+}
