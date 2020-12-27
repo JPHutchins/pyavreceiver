5f43101cd9555dfdaa38ef879e796a0fc6660f47
J.P. Hutchins <jphutchins@gmail.com>
2020-12-26 20:27:17 -0800

    Add kwargs to telnet command

--- tests/test_command.py
+++ tests/test_command.py
@@ -8,4 +8,4 @@ def test_telnet_command():
     """Test telnet command abstract base class."""
     with pytest.raises(TypeError):
         # pylint: disable=abstract-class-instantiated
-        TelnetCommand()
+        TelnetCommand(name="FAKE", command="FAKE", values=None)
