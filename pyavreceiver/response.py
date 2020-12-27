b8cdbbb80eeb55ca2e9f36ddb5170cf3742ae7ed
J.P. Hutchins <jphutchins@gmail.com>
2020-12-26 20:24:10 -0800

    Add method to return name of command

--- pyavreceiver/response.py
+++ pyavreceiver/response.py
@@ -9,22 +9,23 @@ class Message(ABC):
     @abstractmethod
     def message(self) -> str:
         """Return the message."""
-        raise NotImplementedError
 
     @property
     @abstractmethod
     def raw_value(self) -> str:
         """Return the raw value from the message."""
-        raise NotImplementedError
 
     @property
     @abstractmethod
     def state_update(self) -> dict:
         """Return the derived state update."""
-        raise NotImplementedError
 
     @property
     @abstractmethod
     def command(self) -> str:
         """Return the command and parameter category."""
-        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """Return the name. Maybe the same as command."""
