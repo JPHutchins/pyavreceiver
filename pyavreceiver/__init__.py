e4a8ee5eece893c0055dbe41eb78eeb02a2e30e5
J.P. Hutchins <jphutchins@gmail.com>
2021-01-04 17:53:42 -0800

    Keep response reads in the session

--- pyavreceiver/__init__.py
+++ pyavreceiver/__init__.py
@@ -15,22 +15,23 @@ from pyavreceiver.error import AVReceiverIncompatibleDeviceError
 
 async def factory(host: str):
     """Return an instance of an AV Receiver."""
+    names, tasks = [], []
     async with aiohttp.ClientSession() as session:
-        names, tasks = [], []
         for name, url in const.UPNP_ENDPOINTS.items():
             names.append(name)
             tasks.append(
                 asyncio.create_task(session.get(f"http://{host}{url}", timeout=5))
             )
-    for name, response in zip(names, await asyncio.gather(*tasks)):
-        if response.status == 200:
-            if name == "denon-avr-x-2016":
-                http_api = DenonAVRX2016Api(host, await response.text())
-                return DenonReceiver(host, http_api=http_api)
-            if name == "denon-avr-x":
-                http_api = DenonAVRXApi(host, await response.text())
-                return DenonReceiver(host, http_api=http_api)
-            if name == "denon-avr":
-                http_api = DenonAVRApi(host, await response.text())
-                return DenonReceiver(host, http_api=http_api)
-    raise AVReceiverIncompatibleDeviceError
+        responses = await asyncio.gather(*tasks)
+        for name, response in zip(names, responses):
+            if response.status == 200:
+                if name == "denon-avr-x-2016":
+                    http_api = DenonAVRX2016Api(host, await response.text())
+                    return DenonReceiver(host, http_api=http_api)
+                if name == "denon-avr-x":
+                    http_api = DenonAVRXApi(host, await response.text())
+                    return DenonReceiver(host, http_api=http_api)
+                if name == "denon-avr":
+                    http_api = DenonAVRApi(host, await response.text())
+                    return DenonReceiver(host, http_api=http_api)
+        raise AVReceiverIncompatibleDeviceError
