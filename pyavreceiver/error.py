0559add9435566911a8e655e21cd9cd80663f35e
J.P. Hutchins <jphutchins@gmail.com>
2021-01-04 22:00:53 -0800

    Add QoS error

--- pyavreceiver/error.py
+++ pyavreceiver/error.py
@@ -1,9 +1,21 @@
 """pyavreceiver errors."""
 
 
-class AVReceiverInvalidArgumentError(Exception):
+class AVReceiverError(Exception):
+    """Base class for library errors."""
+
+
+class AVReceiverInvalidArgumentError(AVReceiverError):
     """Invalid argument error."""
 
 
-class AVReceiverIncompatibleDeviceError(Exception):
+class AVReceiverIncompatibleDeviceError(AVReceiverError):
     """Invalid argument error."""
+
+
+class QosTooHigh(AVReceiverError):
+    """QoS too high error."""
+
+    def __init__(self):
+        self.message = "Highest QoS value is reserved for resent commands."
+        super().__init__(self.message)
