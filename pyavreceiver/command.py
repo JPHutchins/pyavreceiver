5395fee754c6c2750bd2ef0a07d85b6f1914818c
J.P. Hutchins <jphutchins@gmail.com>
2021-01-04 22:00:12 -0800

    Add __slots__ and treat TelnetCommand as ABC

--- pyavreceiver/command.py
+++ pyavreceiver/command.py
@@ -1,6 +1,6 @@
 """Define commands."""
 from abc import ABC, abstractmethod
-from typing import Sequence, Tuple, Union
+from typing import Callable, Sequence, Tuple, Union
 
 from pyavreceiver.error import AVReceiverInvalidArgumentError
 
@@ -13,6 +13,8 @@ def identity(arg, **kwargs):
 
 class CommandValues:
     """Possible values for a command."""
+
+    __slots__ = ("_values",)
 
     def __init__(self, values: dict):
         self._values = values
@@ -34,7 +36,7 @@ class CommandValues:
     def update(self, _dict):
         """Patch to dict.update()."""
         self._values.update(_dict)
-    
+
     def items(self) -> Sequence[Tuple[str, Union[int, str, float]]]:
         """Patch to dict.items()."""
         return self._values.items()
@@ -55,21 +57,41 @@ class CommandValues:
             self._values[name] = val
 
 
-class TelnetCommand(ABC):
+class Command(ABC):
+    """Command base class."""
+
+
+class TelnetCommand(Command, ABC):
     """Define the telnet command interface."""
+
+    __slots__ = (
+        "_name",
+        "_command",
+        "_values",
+        "_val_pfx",
+        "_func",
+        "_zero",
+        "_val",
+        "_valid_strings",
+        "_message",
+        "_qos",
+        "_sequence",
+    )
 
     def __init__(
         self,
         *,
-        name: str,
-        command: str,
-        values: CommandValues,
+        name: str = None,
+        command: str = None,
+        values: CommandValues = None,
         val_pfx: str = "",
-        func=identity,
+        func: Callable = identity,
         zero: int = 0,
-        val=None,
+        val: Union[float, int, str] = None,
         valid_strings: list = None,
         message: str = None,
+        qos: int = 0,
+        sequence: int = -1
     ):
         self._name = name
         self._command = command
@@ -80,36 +102,72 @@ class TelnetCommand(ABC):
         self._val = val
         self._valid_strings = valid_strings
         self._message = message
+        self._qos = qos
+        self._sequence = sequence
+        self._retries = [0, 1, 2, 2, 2][qos]
+
+    def __hash__(self):
+        return self._sequence
+
+    def __eq__(self, other):
+        try:
+            return self._sequence == other._sequence
+        except AttributeError:
+            return False
 
     @abstractmethod
-    def set_val(self, val) -> str:
+    def set_val(self, val: Union[int, float, str], qos: int = None, sequence: int = -1):
         """Format the command with argument and return."""
 
     @abstractmethod
-    def set_query(self) -> str:
+    def set_query(self, qos: int = None) -> str:
         """Format the command with query and return."""
 
+    def set_sequence(self, sequence) -> None:
+        """Set the sequence to use as hash and id."""
+        self._sequence = sequence
+
+    def lower_qos(self):
+        """Lower the QoS level by one."""
+        self._qos -= 1
+
+    def raise_qos(self):
+        """Raise the QoS level by one."""
+        self._qos += 1
+
     @property
-    @abstractmethod
     def command(self) -> str:
         """The command portion of the message."""
+        return self._command
 
     @property
-    @abstractmethod
     def message(self) -> str:
         """The complete message; command + argument."""
+        if not self._message:
+            raise Exception
+        return self._message
 
     @property
-    @abstractmethod
     def name(self) -> str:
         """The name of the command."""
+        return self._name
 
     @property
-    @abstractmethod
+    def retries(self) -> int:
+        """The number of retries to attempt if the command fails."""
+        return self._retries
+
+    @property
     def val(self) -> str:
         """The argument of the command."""
+        return self._val
 
     @property
-    @abstractmethod
     def values(self) -> list:
         """Return the valid argument values."""
+        return self._values
+
+    @property
+    def qos(self) -> int:
+        """Return the QoS level."""
+        return self._qos
